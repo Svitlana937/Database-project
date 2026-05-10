9fb988bb78653d8d9967e39ef95f0e83ccf98f11
Svitlana <G00473000@atu.ie>
2026-05-10 15:02:02 +0000

    added db_queries in main.py, also fixed few bugs in code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import interface
+import db_queries
+import validators
 
 def run():
     status = True
@@ -12,10 +14,10 @@ def run():
             print(f"\n--- Results for '{name}' ---")
             for speaker, title, room in results:
                 print(f"[*] {speaker} | Session: {title} | Room: {room}")
+
         elif choice == '2':
-            elif choice == '2':
             comp_id = input("Enter Company ID: ")
-            results = db_queries.fetch_attendees_by_company(comp_id)
+            results = db_queries.attendees_by_company(comp_id)
             
             if len(results) == 0:
                 print("No records found.")
@@ -25,7 +27,6 @@ def run():
                     print(row[0], "|", row[1], "|", row[2], "|", row[3])
 
         elif choice == '3':
-            elif choice == '3':
                 id = input("Enter attendee ID: ")
                 name = input("Enter attendee name: ")
                 dob = input("Enter date of birth (YYYY-MM-DD): ")
@@ -37,9 +38,10 @@ def run():
             attendee_id = input("Enter Attendee ID : ")
             
             if attendee_id.isdigit():
-                db_operations.view_connected_attendees(attendee_id)
+                db_queries.view_connected_attendees(attendee_id)
             else:
                 print("*** ERROR *** Attendee ID must be a number")
+
         elif choice == '5':
             while True:
                 id1 = input("Enter Attendee 1 ID : ")
@@ -50,10 +52,12 @@ def run():
                 elif id1 == id2:
                     print("*** ERROR *** An attendee cannot connect to him/herself")
                 else:
-                    db_operations.add_attendee_connection(id1, id2)
+                    db_queries.add_attendee_connection(id1, id2)
                     break
+
         elif choice == '6':
-            db_operations.view_rooms()
+            db_queries.view_rooms()
+            
         elif choice == 'x':
             print("Closing application.")
             status = False
